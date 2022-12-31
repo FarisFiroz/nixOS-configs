c28c78f06092bcbf9c64ac6951d8ca0e30a41c45
FarisFiroz <77905112+FarisFiroz@users.noreply.github.com>
2022-12-30 21:04:08 -0500

    Completely redo nixos config

--- configuration.nix
+++ configuration.nix
@@ -1,115 +1,225 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
+let
+
+  myEmacs = (pkgs.emacsPackagesFor pkgs.emacs).withPackages (epkgs: with epkgs; [ 
+    vterm
+    use-package
+  ]);
+
+  myRetroarch = pkgs.retroarch.override { cores= with pkgs.libretro; [
+    mgba
+    citra
+    mupen64plus
+  ];};
+
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+
+  unstable = import <nixos-unstable> {};
+
+  unfree = import <nixos> {
+    config.allowUnfree = true;
+    overlays = [
+      (final: prev: { 
+        #blender = prev.blender.override { cudaSupport = true; };
+      })
+    ];
+  };
+
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec "$@"
+  '';
+
+in 
 {
+
+  # ===== Base Settings ===== #
+  # ----- Imports ----- #
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
+      (import "${home-manager}/nixos")
     ];
 
-  # Use the systemd-boot EFI boot loader.
+  # ----- Bootloader ----- #
   boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  #boot.loader.efi.canTouchEfiVariables = true;
+  #boot.loader.efi.efiSysMountPoint = "/mnt/boot/efi";
 
-  # networking.hostName = "nixos"; # Define your hostname.
-   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  # ----- Power Management ----- #
+  powerManagement = {
+    enable = true;
+    cpuFreqGovernor = "powersave";
+    powertop.enable = true;
+  };
 
-  # Set your time zone.
-   time.timeZone = "US/Eastern";
+  # ----- Kernel Version ----- #
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # ----- Networking ----- #
+  networking.hostName = "nixos"; # Define your hostname.
+  networking.networkmanager.enable = true;
+
+  # ----- Bluetooth ----- #
+  hardware.bluetooth.enable = true;
+
+  # ----- Locale ----- #
+  time.timeZone = "America/New_York";
+  i18n.defaultLocale = "en_US.utf8";
+
+  # ----- GPU Passthrough ----- #
+   #boot.kernelParams = ["intel_iommu=on"];
+   #boot.blacklistedKernelModules = [ "nvidia" "nouveau" ];
+   #boot.kernelModules = [ "vfio_virqfd" "vfio_pci" "vfio_iommu_type1" "vfio" ];
+   #boot.extraModprobeConfig = "options vfio-pci ids=10de:2520,10de:228e";
+
+  # ----- iGPU Virtualization ----- #
+    #virtualisation.kvmgt.enable = true;
+    #virtualisation.kvmgt.vgpus = {
+    #  "i915-GVTg_V5_4" = {
+    #    uuid = [ "a297db4a-f4c2-11e6-90f6-d3b88d6c9525" ];
+    #  };
+    #};
+  # ===== GUI ===== #
+
+  # ----- X11 ----- #
+  services.xserver.enable = true;
 
 
-  # Select internationalisation properties.
-   i18n.defaultLocale = "en_US.UTF-8";
-   #console = {
-   #  font = "Lat2-Terminus16";
-   #  keyMap = "us";
-   #  useXkbConfig = true; # use xkbOptions in tty.
-   #};
-
-  # Enable the X11 windowing system.
-   services.xserver.enable = true;
-
-  # Enable DE/WM
-  services.xserver.desktopManager.pantheon.enable = true;
-  services.pantheon.apps.enable = false;
-  services.xserver.windowManager.awesome.enable = true;
-
-  # Configure keymap in X11
-   services.xserver.layout = "us";
-   #services.xserver.xkbOptions = {
-   #  "eurosign:e";
-   #  "caps:escape" # map caps to escape.
-   #};
-
-
-  # Enable sound.
-   sound.enable = true;
-   hardware.pulseaudio.enable = true;
-
-  # Make users immutable
-  users.mutableUsers = false;
-  
-  # Set root password
-  users.users.root.initialPassword = "AdminPass";
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-   users.users.shado = {
-     isNormalUser = true;
-     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-     packages = with pkgs; [
-	alacritty
-	librewolf
-	fish
-	htop
-     ];
-   };
-  users.users.shado.initialPassword = "LoginPass";
-  
-  # Enable nonfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # Install nvidia drivers
-  services.xserver.videoDrivers = ["nvidia"];
+  # ----- Install nvidia drivers ----- #
+  boot.blacklistedKernelModules = ["nouveau"];
+  hardware.nvidia.modesetting.enable = true;
+  services.xserver.videoDrivers = ["nvidia"]; 
+  hardware.nvidia.prime = {
+  offload.enable = true;
+  intelBusId = "PCI:0:2:0";
+  nvidiaBusId = "PCI:1:0:0";
+  };
+  hardware.nvidia.powerManagement.enable = true;
   hardware.opengl.enable = true;
 
-  # List packages installed in system profile. To search, run: $ nix search wget
-   environment.systemPackages = with pkgs; [
-     vim
-   ];
+  #specialisation = {
+  #  external-display.configuration = {
+  #    system.nixos.tags = [ "external-display" ];
+  #    hardware.nvidia.prime.offload.enable = lib.mkForce false;
+  #    hardware.nvidia.powerManagement.enable = lib.mkForce false;
+  #  };
+  #}; 
 
+  # ----- Desktop Environment ----- #
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  programs.light.enable = true;
+  programs.sway = {
+    enable = true;
+    extraOptions = ["--unsupported-gpu"];
+    extraPackages = with pkgs; [
+      font-awesome
+      xwayland
+      waybar
+      wofi
+      foot
+      pulsemixer
+    ];
+  };
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # ===== Sound ===== #
+  sound.enable = true;
+  #hardware.pulseaudio.enable = true;
+   security.polkit.enable = true;
+   security.rtkit.enable = true;
+   services.pipewire = {
+     enable = true;
+     alsa.enable = true;
+     alsa.support32Bit = true;
+     pulse.enable = true;
+     jack.enable = true;
+   };
+
+  # ===== Users/Packages ===== #
+
+  # ----- Package Config ----- #
+  nixpkgs = {
+    config = {
+      allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+        "nvidia-x11"
+        "nvidia-settings"
+      ];
+    };
+  };
+
+  # ----- Users ----- #
+  users.users.faris = {
+    isNormalUser = true;
+    description = "Faris";
+    extraGroups = [ "networkmanager" "wheel" "libvirtd" "video"];
+    packages = with pkgs; [
+	myRetroarch
+        keepassxc
+  	unstable.ani-cli
+	ungoogled-chromium
+	unfree.blender
+	gimp
+	myEmacs
+
+    ];
+  };
+
+
+  home-manager.users.faris = {
+    home.stateVersion = "22.11";
+  };
+
+  # ----- System Packages ----- #
+  environment.systemPackages = with pkgs; [
+    powertop
+    qjackctl
+    qpwgraph
+    nvidia-offload
+    unfree.nvtop
+    pciutils
+    libreoffice
+    git
+    vim 
+    librewolf
+    htop
+    neofetch
+    virt-manager
+    mullvad-vpn
+    #btrfs-heatmap
+  ];
+
+  # ----- Emacs ----- #
+
+
+  # ===== Other ===== #
+
+  # ----- Virtualization ----- #
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu.ovmf.enable = true;
+      qemu.verbatimConfig = ''user = "faris"'';
+    };
+  };
+
+ # ----- VPN ----- #
+ services.mullvad-vpn.enable = true;
+  
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  services.xserver.libinput.enable = true;
 
   system.stateVersion = "22.05";
-
 }
-  # Unused:
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
